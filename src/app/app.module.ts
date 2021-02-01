cb837cbd31cfff01b2421763567912d790364ef8
samfan01 <dsamfan@gmail.com>
2021-02-01 23:30:24 +0300

    Created the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BodyComponent } from './body/body.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BodyComponent
+    BodyComponent,
+    QuotesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
