424eb04a84edeb60715f073f268ba6f63d32a508
samfan01 <dsamfan@gmail.com>
2021-02-04 01:47:00 +0300

    This commit updates quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,14 @@
 export class Quote {
-  showDescription: boolean;
-  constructor(public name: string,public quote: string,public vote: string){
+  public upvote=0;
+  public downvote=0;
+
+  showDescription?: boolean;
+  constructor(
+    public name: string,
+    public quote: string,
+    public author:string,
+    public date: Date = new Date()
+    ){
     this.showDescription=false;
   }
 }
